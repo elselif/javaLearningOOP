2a8426a17c269ac901d64f3560c4242b0f81a3e1
Elif <2003013054@student.beykent.edu.tr>
2023-01-01 23:54:41 +0300

    create project
    learn access modifier

---
+++ myJava/src/first/First.java
@@ -0,0 +1,12 @@
+package first;
+
+public class First {
+     
+     public String str;
+
+     First (String str) {
+          this.str = str;
+     }
+
+
+}
---
+++ myJava/src/first/Main.java
@@ -0,0 +1,9 @@
+package first;
+
+public class Main {
+     public static void main(String[] args) {
+          First a = new First("Hello");
+
+          System.out.println(a.str);
+     }
+}
