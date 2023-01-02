7bc4daff1c48261fdaf0e68f578a9bd284877d01
Elif <2003013054@student.beykent.edu.tr>
2023-01-02 03:59:44 +0300

    çakma getter ve setter metodları

--- myJava/oop/src/App.java
+++ myJava/oop/src/App.java
@@ -1,5 +1,15 @@
 public class App {
+
+    // Encapsulation (kapsülleme)
+    // Inharitance (Kalıtım)
+    // Polymorphism (Çok biçimlilik)
+    // Abstraction (Soyutlama)
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Book harryPotter = new Book("Harry Potter", 500);
+        Book lordOfTheRings = new Book("Lord of the Rings", 700);
+
+        
+
     }
 }
